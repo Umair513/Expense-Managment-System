1898b27323c97c60d6e158307d23eea852a548cc
Umair513 <soomroumair513@gmail.com>
2024-05-30 10:25:03 +0500

    created server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const cors = require("cors")
+const morgan = require("morgan")
+const dotenv = require("dotenv")
+const colors = require("colors")
+
+dotenv.config()
+const app = express()
+
+app.use(cors())
+app.use(morgan("dev"))
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello from Server</h1>")
+})
+
+const PORT = 8080 || process.env.PORT
+
+app.listen(PORT, () => {
+    console.log(`Server rinning on ${PORT}`)
+})
