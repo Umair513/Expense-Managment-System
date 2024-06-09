1bc70ea133a6bee9f3cde254d5ca960055c9a665
Umair513 <soomroumair513@gmail.com>
2024-06-09 10:53:43 +0500

    Layout and Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div className="container-fluid">
-      <h1 className="text-center">Hello</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage></HomePage>}></Route>
+      </Routes>
+    </>
   );
 }
 
